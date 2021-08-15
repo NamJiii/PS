009c3d079295a57049167082bd6bafecb4ae20cc
남지훈 <2017029561@hanynag.ac.kr>
2021-08-15 18:03:31 +0900

    1006 time limit

--- 1006.py
+++ 1006.py
@@ -1,57 +1,69 @@
 #1006
 '''
 정답은 N ~ 2*N
-1. 주변에 합쳐서 100이내로 할 수 없는것들은 미리 빼둔다.
-2. 근처에 이웃수가 1인것부터 쭉 뺀다.
+1. 이어질수 있는 bridge 위치들 저장
+2. 근처에 이웃수가 적은것부터 쭉 뺀다.
 '''
 
 def BOJ1006():
     N,W =map(int,input().split())
     arr1 = list(map(int,input().split()))
     arr2 = list(map(int,input().split()))
-    bridge = [[0 for _ in range(N)] for _ in range (3)]
+
     bridge_list = []
-    #bridge 를 채운다.
+    #bridge_list 채우기
     for i in range(N):
         if (arr1[i] + arr1[i - 1] <= W):
-            bridge[0][i] = 1
             bridge_list.append([0,i])
         if (arr2[i] + arr2[i - 1] <= W):
-            bridge[2][i] = 1
             bridge_list.append([2, i])
         if (arr1[i - 1] + arr2[i - 1] <= W):
-            bridge[1][i] = 1
             bridge_list.append([1, i])
-    #위에서부터 하나씩 삭제ㄱㄱ
+
+    #하나씩 삭제ㄱㄱ
     couple_count=0
+
     while(bridge_list):
+        prev_bridge_list = bridge_list.copy()
+
+        less_neighbor = 10 # 7~8을 넘을수 없다
+        less_neighbor_list = []
+
         for a,b in bridge_list:
-            neighbor = 0
-            neighbor_id = []
-            if a-1>=0 and b-1>=0 and bridge[a-1][b-1]==1:
-                neighbor = neighbor + 1
-                neighbor_id = [a-1,b-1]
-            if a - 1 >= 0 and bridge[a - 1][b] == 1:
-                neighbor = neighbor + 1
-                neighbor_id = [a - 1,b]
-            if b - 1 >= 0 and bridge[a][b - 1] == 1:
-                neighbor = neighbor + 1
-                neighbor_id = [a - 1, b - 1]
-            if a + 1 < N and b + 1 < N and bridge[a + 1][b + 1] == 1:
-                neighbor = neighbor + 1
-                neighbor_id = [a + 1, b + 1]
-            if a + 1 < N and bridge[a + 1][b] == 1:
-                neighbor = neighbor + 1
-                neighbor_id = [a + 1, b]
-            if b + 1 < N and bridge[a][b + 1] == 1:
-                neighbor = neighbor + 1
-                neighbor_id = [a, b + 1]
-            if neighbor==0 or neighbor==1:
+            neighbor_id = [[a,b]]
+
+            for i,j in bridge_list:
+                if (a - i)*(a - i) <=1 :
+                    if (b - j)*(b - j) <= 1 and (a-i) != (b-j):
+                        pass
+                    elif (b - j - N)*(b - j - N) <= 1 and (a-i) != (b-j-N):
+                        pass
+                    elif (b - j + N)*(b - j + N) <= 1 and (a-i) != (b-j+N):
+                        pass
+                    else:
+                        continue
+                    neighbor_id.append([i,j])
+                else :
+                    continue
+
+            if len(neighbor_id)==1 or len(neighbor_id)==2:
                 couple_count= couple_count+1
-                bridge_list.remove([a,b])
-                if neighbor_id:
-                    bridge_list.remove(neighbor_id)
-    print(couple_count)
+                for i in neighbor_id:
+                    if i in bridge_list:
+                        bridge_list.remove(i)
+            else :
+                if len(neighbor_id)<less_neighbor:
+                    less_neighbor = len(neighbor_id)
+                    less_neighbor_list = neighbor_id
+
+        if prev_bridge_list == bridge_list:
+            couple_count = couple_count+1
+            for i in less_neighbor_list:
+                if i in bridge_list:
+                    bridge_list.remove(i)
+
+    print(2*N-couple_count)
+    return
 
 
 
@@ -61,6 +73,7 @@ def main():
     while(T!=0):
         T = T-1
         BOJ1006()
+    return
 
-if __name__ =="__main__":
-    main()
+#if __name__ =="__main__":
+main()
